1e7c795af14f995ab8f1ae7b2c60d91547567966
Hazey8709 <jfortin802@gmail.com>
2023-11-21 23:42:16 -0500

    ud 4 setUp

---
+++ W3-04_AddClass/notes.js
@@ -0,0 +1,50 @@
+//!         Add Class & Multiple classes
+
+//* Syntax
+// w3.addClass(selector, 'class')
+//  w3.addClass(selector, 'class1 class2 class3...')
+
+//* Add class By ID:
+// w3.addClass('#London','marked')
+
+//* Add class By Tag Name:
+// w3.addClass('h2','marked')
+
+//* Add Class By Class:
+// w3.addClass('.city','marked')
+
+//* Add Multiple Classes
+// w3.addClass('#London','class1 class2')
+
+//!        Remove Class  By ID:
+
+//* Syntax
+// w3.removeClass(selector,'class')
+// w3.removeClass(selector,'class1 class2 class3...')
+
+//* Remove Class By ID:
+// w3.removeClass('#London','marked')
+
+//* Remove Class BY Class:
+// w3.removeClass('.city','marked')
+
+//* Remove Multiple Classes:
+// w3.removeClass('#London','class1 class2')
+
+//! Toggle the Class of HTML elements
+
+//* Syntax
+//  w3.toggleClass(selector,'class')
+// w3.toggleClass(selector,'property','class','class')
+
+//* Toggle Class By ID:
+// w3.toggleClass('#London','marked')
+
+//* Toggle Class by Tag:
+// w3.toggleClass('h2','marked')
+
+//* Toggle Class by Class:
+// w3.toggleClass('.city','marked')
+
+//* Toggle Multiple Classes:
+// w3.toggleClass('#London','class1','class2')
