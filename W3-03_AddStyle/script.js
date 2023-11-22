acc6f58139708a21eca2b5ff4a4510635a708756
Hazey8709 <jfortin802@gmail.com>
2023-11-21 22:37:59 -0500

    ud #4

--- W3-03_AddStyle/script.js
+++ W3-03_AddStyle/script.js
@@ -1,1 +1,24 @@
-//
+//! Add Style By ID:
+//s Get a reference to the HTML element you want to listen for clicks on
+const addById = document.getElementById("d1_BtnAdd");
+const removeById = document.getElementById("d1_BtnRemove");
+
+//s Define the callback addStyle()
+function addStyle() {
+    w3.addStyle("#div-1", "color", "blue");
+
+    //s Log:
+    // console.log('Clicked');
+}
+
+//s Define the callback removeStyle()
+function removeStyle() {
+    w3.addStyle("#div-1", "color", "black");
+
+    //s Log:
+    // console.log('Clicked');
+}
+
+//s Add the event listener to the button
+addById.addEventListener("click", addStyle);
+removeById.addEventListener("click", removeStyle);
